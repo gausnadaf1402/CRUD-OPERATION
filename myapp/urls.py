3c76cd7bbab5087d878d5e73f9fce8d0663026a8
md gaus <nadafgaus1402@gmail.com>
2023-03-15 13:09:24 +0530

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('add',views.add,name='add'),
+    path('edit',views.edit,name='edit'),
+    path('update/<str:id>',views.update,name='update'),
+    path('delete/<str:id>',views.delete,name='delete'),
+]
